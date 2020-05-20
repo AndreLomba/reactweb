c3045f658fed83affd4b4cc9559c8b7768b2f5f8
André Lomba <landrelomba@gmail.com>
2020-05-20 01:45:09 +0000

    Continuação da aula

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import api from './api';
 
 function App() {
-  return <h1>ReactJS, Template!</h1>
+
+    const [lista, setLista] = useState([]); 
+
+    useEffect(() => {
+        api.get('/agenda').then((response) => {
+            const itens = response.data;
+            setLista(itens);
+        });
+    }, []);
+    return (
+        <table border="1">
+            <tr>
+                <th>id</th>
+                <th>Nome</th>
+                <th>Número</th>
+                <th>Favorito</th>
+
+            </tr>
+            {lista.map(itens => (
+                <tr key={itens.id}>
+                    <td>{itens.id}</td>
+                    <td>{itens.nome}</td>
+                    <td>{itens.numero}</td>
+                    <td>{itens.favorito}/></td>
+                </tr>
+            ))}
+            
+        </table>
+    )
 }
 
 export default App;
